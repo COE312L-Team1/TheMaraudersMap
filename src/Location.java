6de40990edfa19c4f393a1111d9ba4ebb001bcf8
ashithFarhan <b00079866@aus.edu>
2020-09-21 00:40:32 +0400

    Added greatHall; added item/person desc printing in location.look(); added Dumbledore, food and sorting hat; added map layout in comments

--- src/Location.java
+++ src/Location.java
@@ -9,9 +9,20 @@ public class Location
 	void look()
 	{
 		System.out.println(this.desc);
+		//if the location has a person and/or object(s), their descriptions get printed too
+		if(this.p != null) {
+			System.out.println(this.p.desc);
+		}
+		if(this.a != null) {
+			System.out.println(this.a.desc);
+		}
+		if(this.b != null) {
+			System.out.println(this.b.desc);
+		}
+				
 	}
 
-	public Location(Person p, Item a, Item b, Location north, Location south, Location east, Location west, String desc)
+	public Location(String desc, Person p, Item a, Item b, Location north, Location south, Location east, Location west)
 	{
 		this.p = p;
 		this.a = a;
--- src/main.java
+++ src/main.java
@@ -1,13 +1,37 @@
 
 public class main
 {
+	/*				  =====
+	 * 				   MAP			N
+	 * 				  =====		W	+	E
+	 * 								S
+	 * 				Great Hall
+	 * 					^
+	 * 					|
+	 *Library <-->	Entrance  <-->	Potions
+	 * 
+	 * */
 
 	public static void main(String[] args)
 	{
-		Location Entrance = new Location(new Person(), new Item(), new Item(),
-				new Location(), new Location(), new Location(), new Location(),
-				"The Entrance\n\nYou arrive at the doors of Hogwarts. The door on the north wall leads to\r\nthe dining hall, the door to the east leads to the Potions class, and\r\nthe door to the west leads to the Library.");
-		System.out.println("Hello World!");
+		Location Entrance = new Location("\t\tThe Entrance\n\nYou arrive at the doors of Hogwarts. The door on the north wall leads to\r\nthe dining hall, the door to the east leads to the Potions class, and\r\nthe door to the west leads to the Library.",
+							null, null, null, null, null, null, null);
+		//^ all null, cuz no person, no items, and neighbor locations havent been declared yet
+		
+		Location greatHall = new Location("\t\tThe Great Hall\n\nYou are walking up a huge hall with a towering ceiling, lit by thousands of floating\r\ncandles. You see four long tables, one for each house, being set\r\nfor the feast.",
+							 new Person("Dumbledore","In front of you, you see Professor Dumbledore, the headmaster of the Hogwarts School\r\nof Witchcraft and Wizardry. He is dressed in some fine blue robes\r\nand seems to be preparing a speech for tonight."),
+							 new Item("Food","On the tables, you see all sorts of delicious food, from roast chicken to treacle tarts!"),
+							 new Item("Sorting hat","At the end of the hall, on a small wooden stool, sits the legendary Sorting Hat,\r\nstill wrinkled, dirty and frayed."),
+							 null, Entrance, null, null);
+		//^ only neighboring location is entrance, to the south
+		
+		Entrance.north = greatHall;
+		
+		Entrance.look();
+		System.out.println();
+		greatHall.look();
+		
+
 	}
 
 }
