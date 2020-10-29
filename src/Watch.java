e9ecb55e3b543ed43faaa6f98a07ecba82ebad2a
wtfarooq <b00080205@aus.edu>
2020-10-30 00:32:24 +0400

    forward counting for time instead of reverse

--- src/Watch.java
+++ src/Watch.java
@@ -1,34 +1,42 @@
 
 public class Watch implements java.lang.Runnable {
 
+	int i;
+	
+	Watch() { i = 0; }
+	
 	@Override
 	public void run() {
 		this.runTimer();
 	}
 
+	public void timeLeft()
+	{
+		System.out.println("\n" + (180 - i) / 60 + " minutes and " + (180 - i) % 60 + " seconds left");
+	}
+	
 	public void runTimer() {
-		int i = 180;
 		boolean timesup = false;
-		while (i >= 0) {
+		while (i <= 180) {
 			try {
 				if (i == 60 || i == 120) {
 					synchronized (this) {
 						System.out.print("\n" + (i / 60) + " minute");
-						System.out.println(((i == 120) ? "s " : " ") + "remaining");
+						System.out.println(((i == 120) ? "s " : " ") + "passed...");
 					}
 				}
-				if (i == 0 && !timesup) {
+				if (i == 180 && !timesup) {
 					synchronized (this) {
 						timesup = true;
-						System.out.println("Time is up! 10 seconds to hide map contents!");
-						i += 10;
+						System.out.println("\nTime is up! 10 seconds to hide map contents!");
+						i -= 10;
 					}
-				} else if (i == 0 && timesup) {
+				} else if (i == 180 && timesup) {
 					synchronized (this) {
-						System.out.println("YOU WERE CAUGHT! Marauder’s map has been confiscated!");
+						System.out.println("\nYOU WERE CAUGHT! Marauder’s map has been confiscated!");
 					}
 				}
-				i--;
+				i++;
 				Thread.sleep(1000L);
 			} catch (InterruptedException e) {
 				synchronized (this) {
