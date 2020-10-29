bde2784793572cb37674d8270d42579eefd9c187
wtfarooq <b00080205@aus.edu>
2020-10-29 22:55:55 +0400

    changed location of i--

--- src/Watch.java
+++ src/Watch.java
@@ -2,49 +2,37 @@
 public class Watch implements java.lang.Runnable {
 
 	@Override
-	public void run() 
-	{
+	public void run() {
 		this.runTimer();
 	}
 
-	public void runTimer() 
-	{
+	public void runTimer() {
 		int i = 180;
 		boolean timesup = false;
-		while (i > 0) 
-		{
-			try 
-			{
+		while (i > 0) {
+			try {
 				if (i == 60 || i == 120) {
-					synchronized (this)
-					{
-					System.out.print("" + (i / 60) + " minute");
-					System.out.println((i / 120 == 1) ? " " : "s " + "passed");
-					i--;
+					synchronized (this) {
+						System.out.print("" + (i / 60) + " minute");
+						System.out.println((i / 120 == 1) ? " " : "s " + "passed");
 					}
 				}
-				if(i == 0 && !timesup)
-				{
-					synchronized (this)
-					{
-					timesup = true;
-					System.out.println("Time is up! 10 seconds to hide map contents!");
-					i += 10;
+				if (i == 0 && !timesup) {
+					synchronized (this) {
+						timesup = true;
+						System.out.println("Time is up! 10 seconds to hide map contents!");
+						i += 10;
+					}
+				} else if (timesup) {
+					synchronized (this) {
+						System.out.println("YOU WERE CAUGHT! Marauder’s map has been confiscated!");
 					}
 				}
-				else if (timesup)
-				{
-					synchronized (this)
-					{
-					System.out.println("YOU WERE CAUGHT! Marauder’s map has been confiscated!");
-					}
-				}
+				i--;
 				Thread.sleep(1000L);
-			}
-			catch (InterruptedException e) {
-				synchronized (this)
-				{
-				System.out.println(e.getMessage());
+			} catch (InterruptedException e) {
+				synchronized (this) {
+					System.out.println(e.getMessage());
 				}
 			}
 		}
