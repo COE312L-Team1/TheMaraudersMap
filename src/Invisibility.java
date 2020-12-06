dfb60f2c3d6c5db0be78fa1e1b105210f5f1416d
wtfarooq <b00080205@aus.edu>
2020-12-07 00:25:43 +0400

    n/a

--- src/Invisibility.java
+++ src/Invisibility.java
@@ -3,21 +3,18 @@ public class Invisibility extends Juice {
 
 	@Override
 	void Stir() {
-		// TODO Auto-generated method stub
 		System.out.println("Stirring 3 times anticlockwise\n");
 		
 	}
 
 	@Override
 	void Wave() {
-		// TODO Auto-generated method stub
 		
 		
 	}
 
 	@Override
 	void additems() {
-		// TODO Auto-generated method stub
 		System.out.println("Adding cherries and spiders in cauldron\n");
 		
 	}
--- src/Polyjuice.java
+++ src/Polyjuice.java
@@ -3,15 +3,13 @@ public class Polyjuice extends Juice  {
 
 	@Override
 	void Stir() {
-		// TODO Auto-generated method stub
-		
+	
 		System.out.println("Stirring 4 times clockwise\n");
 		
 	}
 
 	@Override
 	void Wave() {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Waving wand to complete the brewing process\n");
 		
@@ -19,7 +17,6 @@ public class Polyjuice extends Juice  {
 
 	@Override
 	void additems() {
-		// TODO Auto-generated method stub
 		System.out.println("Adding fluxweed and lacewings in cauldron\n");
 		
 	}
